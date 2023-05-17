c5affe08e168f891dbb60ee8b5dcfb77d06f4ae7
Tearth <tearthdev@gmail.com>
2023-05-17 23:52:23 +0200

    Add atlas loading

--- orion-core/src/assets/loader.rs
+++ orion-core/src/assets/loader.rs
@@ -1,12 +1,16 @@
 use super::*;
 use crate::filesystem::FileLoadingStatus;
 use crate::filesystem::FileSystem;
+use anyhow::bail;
 use anyhow::Result;
 use png::Decoder;
-use std::io::BufReader;
+use quick_xml::events::Event;
+use quick_xml::Reader;
+use std::borrow::Cow;
 use std::io::Cursor;
 use std::io::Read;
 use std::path::Path;
+use std::str;
 use zip::ZipArchive;
 
 pub struct AssetsLoader {
@@ -16,6 +20,7 @@ pub struct AssetsLoader {
 
     pub raw_textures: Vec<RawTexture>,
     pub raw_fonts: Vec<RawFont>,
+    pub raw_atlases: Vec<RawAtlas>,
 }
 
 impl AssetsLoader {
@@ -26,16 +31,17 @@ impl AssetsLoader {
             filesystem: Default::default(),
             raw_textures: Default::default(),
             raw_fonts: Default::default(),
+            raw_atlases: Default::default(),
         }
     }
 
     pub fn load(&mut self, input: &str) -> Result<AssetsLoadingStatus> {
-        if self.status == AssetsLoadingStatus::Finished && self.input != input {
-            self.status = AssetsLoadingStatus::Idle;
+        if self.status == AssetsLoadingStatus::Idle && self.input != input {
+            self.status = AssetsLoadingStatus::Initializing;
         }
 
         match self.status {
-            AssetsLoadingStatus::Idle => {
+            AssetsLoadingStatus::Initializing => {
                 self.filesystem.load(input)?;
 
                 self.input = input.to_string();
@@ -43,7 +49,9 @@ impl AssetsLoader {
             }
             AssetsLoadingStatus::Loading => {
                 if self.filesystem.load(input)? == FileLoadingStatus::Finished {
-                    let buffer = self.filesystem.buffer.borrow();
+                    let buffer = self.filesystem.buffer.clone();
+                    let buffer = buffer.borrow();
+
                     let slice = buffer.as_slice();
                     let cursor = Cursor::new(slice);
                     let mut archive = ZipArchive::new(cursor)?;
@@ -58,30 +66,106 @@ impl AssetsLoader {
                             let mut data = Vec::new();
                             entry.read_to_end(&mut data)?;
 
-                            if extension == "png" {
-                                let cursor = Cursor::new(data);
-                                let mut decoder = Decoder::new(cursor);
-                                decoder.set_transformations(png::Transformations::normalize_to_color8());
-
-                                let mut reader = decoder.read_info()?;
-                                let mut data = vec![0; reader.output_buffer_size()];
-                                let info = reader.next_frame(&mut data)?;
-                                let size = Vec2::new(info.width as f32, info.height as f32);
-
-                                self.raw_textures.push(RawTexture::new(name, size, &data));
-                            } else if extension == "ttf" {
-                                self.raw_fonts.push(RawFont::new(name, data));
-                            }
+                            match extension.as_str() {
+                                "png" => self.load_png(&name, &data)?,
+                                "ttf" => self.load_ttf(&name, &data)?,
+                                "xml" => self.load_xml(&name, &data)?,
+                                _ => {}
+                            };
                         }
                     }
 
                     self.status = AssetsLoadingStatus::Finished;
                 }
             }
+            AssetsLoadingStatus::Finished => {
+                self.status = AssetsLoadingStatus::Idle;
+            }
             _ => {}
         }
 
         Ok(self.status)
+    }
+
+    fn load_png(&mut self, name: &str, data: &[u8]) -> Result<()> {
+        let cursor = Cursor::new(data);
+        let mut decoder = Decoder::new(cursor);
+        decoder.set_transformations(png::Transformations::normalize_to_color8());
+
+        let mut reader = decoder.read_info()?;
+        let mut data = vec![0; reader.output_buffer_size()];
+        let info = reader.next_frame(&mut data)?;
+        let size = Vec2::new(info.width as f32, info.height as f32);
+
+        self.raw_textures.push(RawTexture::new(name, size, &data));
+
+        Ok(())
+    }
+
+    fn load_ttf(&mut self, name: &str, data: &[u8]) -> Result<()> {
+        self.raw_fonts.push(RawFont::new(name, data));
+        Ok(())
+    }
+
+    fn load_xml(&mut self, name: &str, data: &[u8]) -> Result<()> {
+        let xml = str::from_utf8(data)?;
+        let mut reader = Reader::from_str(xml);
+        reader.trim_text(true);
+        reader.expand_empty_elements(true);
+
+        let mut image = String::new();
+        let mut textures = Vec::new();
+
+        loop {
+            match reader.read_event() {
+                Ok(Event::Start(element)) => match element.name().as_ref() {
+                    b"TextureAtlas" => {
+                        let mut image_path = Cow::Borrowed("");
+
+                        for attr_result in element.attributes() {
+                            let a = attr_result?;
+                            if let b"imagePath" = a.key.as_ref() {
+                                image_path = a.decode_and_unescape_value(&reader)?
+                            }
+                        }
+
+                        image = image_path.to_string();
+                    }
+                    b"SubTexture" => {
+                        let mut name = Cow::Borrowed("");
+                        let mut x = Cow::Borrowed("");
+                        let mut y = Cow::Borrowed("");
+                        let mut width = Cow::Borrowed("");
+                        let mut height = Cow::Borrowed("");
+
+                        for attr_result in element.attributes() {
+                            let a = attr_result?;
+                            match a.key.as_ref() {
+                                b"name" => name = a.decode_and_unescape_value(&reader)?,
+                                b"x" => x = a.decode_and_unescape_value(&reader)?,
+                                b"y" => y = a.decode_and_unescape_value(&reader)?,
+                                b"width" => width = a.decode_and_unescape_value(&reader)?,
+                                b"height" => height = a.decode_and_unescape_value(&reader)?,
+                                _ => (),
+                            }
+                        }
+
+                        let position = Vec2::new(x.parse()?, y.parse()?);
+                        let size = Vec2::new(width.parse()?, height.parse()?);
+
+                        textures.push(RawAtlasTexture::new(&name, position, size));
+                    }
+                    _ => {}
+                },
+                Err(error) => bail!("Error at position {}: {:?}", reader.buffer_position(), error),
+                Ok(Event::Eof) => break,
+                _ => (),
+            }
+        }
+
+        self.raw_atlases.push(RawAtlas::new(name, &image, textures));
+
+        Ok(())
     }
 }
 
--- orion-core/src/assets/mod.rs
+++ orion-core/src/assets/mod.rs
@@ -6,6 +6,7 @@ pub mod loader;
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum AssetsLoadingStatus {
     Idle,
+    Initializing,
     Loading,
     Parsing,
     Finished,
@@ -22,14 +23,38 @@ pub struct RawFont {
     pub data: Vec<u8>,
 }
 
+pub struct RawAtlas {
+    pub name: String,
+    pub image: String,
+    pub textures: Vec<RawAtlasTexture>,
+}
+
+pub struct RawAtlasTexture {
+    pub name: String,
+    pub position: Vec2,
+    pub size: Vec2,
+}
+
 impl RawTexture {
-    pub fn new(name: String, size: Vec2, data: &[u8]) -> Self {
-        Self { name, size, data: data.to_vec() }
+    pub fn new(name: &str, size: Vec2, data: &[u8]) -> Self {
+        Self { name: name.to_string(), size, data: data.to_vec() }
     }
 }
 
 impl RawFont {
-    pub fn new(name: String, data: Vec<u8>) -> Self {
-        Self { name, data }
+    pub fn new(name: &str, data: &[u8]) -> Self {
+        Self { name: name.to_string(), data: data.to_vec() }
+    }
+}
+
+impl RawAtlas {
+    pub fn new(name: &str, image: &str, textures: Vec<RawAtlasTexture>) -> Self {
+        Self { name: name.to_string(), image: image.to_string(), textures }
+    }
+}
+
+impl RawAtlasTexture {
+    pub fn new(name: &str, position: Vec2, size: Vec2) -> Self {
+        Self { name: name.to_string(), position, size }
     }
 }

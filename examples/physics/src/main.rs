b0132951dc052fdef93a5f3e0096befdc5246e96
Tearth <tearthdev@gmail.com>
2023-06-18 14:04:48 +0200

    Add car to physics example

--- examples/physics/src/main.rs
+++ examples/physics/src/main.rs
@@ -13,6 +13,7 @@ use orion_core::egui::RichText;
 use orion_core::egui::SidePanel;
 use orion_core::fast_gpu;
 use orion_core::glam::Vec2;
+use orion_core::glam::Vec4;
 use orion_core::rapier2d::prelude::*;
 use orion_core::renderer::sprite::Sprite;
 use orion_core::scene::FrameCommand;
@@ -25,7 +26,7 @@ use std::collections::VecDeque;
 
 fast_gpu!();
 
-const COUNT: usize = 100;
+const COUNT: usize = 20;
 const DELTA_HISTORY_COUNT: usize = 100;
 const PIXELS_PER_METER: f32 = 50.0;
 
@@ -40,11 +41,18 @@ struct MainScene {
 
     terrain: Sprite,
     terrain_collider: Option<ColliderHandle>,
+
+    wheel_left: Sprite,
+    wheel_right: Sprite,
+    wheel_left_rigidbody: RigidBodyHandle,
+    wheel_right_rigidbody: RigidBodyHandle,
+
+    car: Sprite,
+    car_rigidbody: RigidBodyHandle,
 }
 
 struct Object {
     sprite: Sprite,
-    collider: ColliderHandle,
     rigidbody: RigidBodyHandle,
 }
 
@@ -75,6 +83,18 @@ impl Scene<GlobalData> for MainScene {
 
         println!("Collisions: {}, contacts: {}", collisions.len(), contacts.len());
 
+        if state.window.keyboard_state[Key::KeyA as usize] {
+            state.physics.rigidbodies.get_mut(self.wheel_left_rigidbody).unwrap().apply_torque_impulse(0.02, true);
+        }
+
+        if state.window.keyboard_state[Key::KeyD as usize] {
+            state.physics.rigidbodies.get_mut(self.wheel_left_rigidbody).unwrap().apply_torque_impulse(-0.02, true);
+        }
+
+        if state.window.keyboard_state[Key::Space as usize] {
+            state.physics.rigidbodies.get_mut(self.wheel_left_rigidbody).unwrap().set_angvel(0.0, true);
+        }
+
         Ok(None)
     }
 
@@ -93,6 +113,31 @@ impl Scene<GlobalData> for MainScene {
             self.terrain = Sprite { size: Some(Vec2::new(state.renderer.viewport_size.x, 50.0)), ..Default::default() };
             self.terrain_collider = Some(state.physics.colliders.insert(ColliderBuilder::cuboid(100.0, 0.1).build()));
 
+            self.wheel_left = Sprite { texture_id: Some(state.renderer.textures.get_by_name("wheel")?.id), ..Default::default() };
+            self.wheel_right = Sprite { texture_id: Some(state.renderer.textures.get_by_name("wheel")?.id), ..Default::default() };
+            self.car = Sprite { size: Some(Vec2::new(100.0, 50.0)), color: Vec4::new(0.8, 0.8, 0.8, 1.0), ..Default::default() };
+
+            let collider = ColliderBuilder::ball(0.3).restitution(0.7).build();
+            let rigidbody = RigidBodyBuilder::dynamic().translation(vector![300.0, 300.0] / PIXELS_PER_METER).build();
+            self.wheel_left_rigidbody = state.physics.rigidbodies.insert(rigidbody);
+            let collider_handle = state.physics.colliders.insert_with_parent(collider, self.wheel_left_rigidbody, &mut state.physics.rigidbodies);
+
+            let collider = ColliderBuilder::ball(0.3).restitution(0.7).build();
+            let rigidbody = RigidBodyBuilder::dynamic().translation(vector![350.0, 300.0] / PIXELS_PER_METER).build();
+            self.wheel_right_rigidbody = state.physics.rigidbodies.insert(rigidbody);
+            let collider_handle = state.physics.colliders.insert_with_parent(collider, self.wheel_right_rigidbody, &mut state.physics.rigidbodies);
+
+            let collider = ColliderBuilder::cuboid(1.0, 0.5).restitution(0.7).build();
+            let rigidbody = RigidBodyBuilder::dynamic().translation(vector![325.0, 320.0] / PIXELS_PER_METER).build();
+            self.car_rigidbody = state.physics.rigidbodies.insert(rigidbody);
+            let collider_handle = state.physics.colliders.insert_with_parent(collider, self.car_rigidbody, &mut state.physics.rigidbodies);
+
+            let joint = RevoluteJointBuilder::new().local_anchor1(point![-0.6, -0.5]).local_anchor2(point![0.0, 0.0]).contacts_enabled(false);
+            state.physics.impulse_joints.insert(self.car_rigidbody, self.wheel_left_rigidbody, joint, true);
+
+            let joint = RevoluteJointBuilder::new().local_anchor1(point![0.6, -0.5]).local_anchor2(point![0.0, 0.0]).contacts_enabled(false);
+            state.physics.impulse_joints.insert(self.car_rigidbody, self.wheel_right_rigidbody, joint, true);
+
             for _ in 0..COUNT {
                 let position = Vec2::new(
                     fastrand::u32(0..state.renderer.viewport_size.x as u32) as f32,
@@ -107,7 +152,7 @@ impl Scene<GlobalData> for MainScene {
                 let rigidbody_handle = state.physics.rigidbodies.insert(rigidbody);
                 let collider_handle = state.physics.colliders.insert_with_parent(collider, rigidbody_handle, &mut state.physics.rigidbodies);
 
-                self.objects.push(Object { sprite, collider: collider_handle, rigidbody: rigidbody_handle });
+                self.objects.push(Object { sprite, rigidbody: rigidbody_handle });
             }
 
             self.initialized = true;
@@ -123,6 +168,24 @@ impl Scene<GlobalData> for MainScene {
                     self.objects[i].sprite.rotation = interpolation_data.get_rotation_interpolated(alpha);
                     state.renderer.draw_sprite(&self.objects[i].sprite)?;
                 }
+            }
+
+            if let Some(interpolation_data) = state.physics.interpolation_data.get(&self.wheel_left_rigidbody) {
+                self.wheel_left.position = interpolation_data.get_position_interpolated(alpha) * PIXELS_PER_METER;
+                self.wheel_left.rotation = interpolation_data.get_rotation_interpolated(alpha);
+                state.renderer.draw_sprite(&self.wheel_left)?;
+            }
+
+            if let Some(interpolation_data) = state.physics.interpolation_data.get(&self.wheel_right_rigidbody) {
+                self.wheel_right.position = interpolation_data.get_position_interpolated(alpha) * PIXELS_PER_METER;
+                self.wheel_right.rotation = interpolation_data.get_rotation_interpolated(alpha);
+                state.renderer.draw_sprite(&self.wheel_right)?;
+            }
+
+            if let Some(interpolation_data) = state.physics.interpolation_data.get(&self.car_rigidbody) {
+                self.car.position = interpolation_data.get_position_interpolated(alpha) * PIXELS_PER_METER;
+                self.car.rotation = interpolation_data.get_rotation_interpolated(alpha);
+                state.renderer.draw_sprite(&self.car)?;
             }
         }
 

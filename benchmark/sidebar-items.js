0c50c71fc9e68862bd2b3e4d127fa4c88b90ab40
Tearth <Tearth@users.noreply.github.com>
2023-09-28 19:10:15 +0000

    deploy: 792d280c1501f4352fbcf1f11b7fa2fff4478fb7

--- benchmark/sidebar-items.js
+++ benchmark/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["main","main_internal"],"static":["AmdPowerXpressRequestHighPerformance","NvOptimusEnablement"],"struct":["GlobalData","MainScene","Object"]};
+window.SIDEBAR_ITEMS = {"enum":["SelectedShader"],"fn":["main","main_internal"],"static":["AmdPowerXpressRequestHighPerformance","NvOptimusEnablement"],"struct":["GlobalData","MainScene","Object"]};

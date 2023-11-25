69dbf6c5ec33dd1719cba1de167947563c16dd88
Tearth <Tearth@users.noreply.github.com>
2023-11-25 15:04:24 +0000

    deploy: 880045e4a4e941b79ebecfa12a9940956a96b8f1

--- network_base/sidebar-items.js
+++ network_base/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {};
+window.SIDEBAR_ITEMS = {"constant":["PACKET_OBJECTS_ARRAY","PACKET_SET_COUNT","PACKET_SET_VIEWPORT","TICK"],"struct":["PacketSetCount","PacketSetViewport"]};

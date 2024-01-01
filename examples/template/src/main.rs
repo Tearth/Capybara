a6acaec9a5a6692b8822c541b77f7cb0d1d35f4d
Tearth <tearthdev@gmail.com>
2024-01-01 15:42:25 +0100

    Hide console in release mode

--- examples/template/src/main.rs
+++ examples/template/src/main.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::too_many_arguments, clippy::collapsible_if)]
+#![cfg_attr(all(target_os = "windows", not(debug_assertions)), windows_subsystem = "windows")]
 
 use capybara::anyhow::Result;
 use capybara::app::ApplicationContext;

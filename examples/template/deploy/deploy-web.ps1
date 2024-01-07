c3e7b4435541bea38f0e2f61bf65b857baaef640
Tearth <tearthdev@gmail.com>
2024-01-07 14:10:40 +0100

    Remove path replacing (trunk has been fixed)

--- examples/template/deploy/deploy-web.ps1
+++ examples/template/deploy/deploy-web.ps1
@@ -1,13 +1,8 @@
 $ZIP = "web"
 
 Write-Output "Building Web binary..."
-Start-Process -FilePath "trunk" -ArgumentList "build --release" -Wait -NoNewWindow
+Start-Process -FilePath "trunk" -ArgumentList "build --release --public-url ./" -Wait -NoNewWindow
 Set-Location .\dist\
-
-Write-Output "Replacing paths..."
-Get-ChildItem -Path .\ -Filter *.html -File -Name | ForEach-Object {
-	(Get-Content $_) -replace """/", """./" -replace "'/", "'./" | Out-File -encoding ASCII $_
-}
 
 Write-Output "Optimizing WASM..."
 Get-ChildItem -Path .\ -Filter *.wasm -File -Name | ForEach-Object {

025edce5a69a99a9ab86ecaeb1f7ff2b454ba0f4
Tearth <tearthdev@gmail.com>
2024-02-07 21:09:52 +0100

    Fix duplicated targets

--- examples/network/template/worker/build.rs
+++ examples/network/template/worker/build.rs
@@ -2,8 +2,6 @@ use cfg_aliases::cfg_aliases;
 
 fn main() {
     cfg_aliases! {
-        windows: { all(target_os = "windows", target_arch = "x86_64") },
-        unix: { all(target_os = "linux", target_arch = "x86_64") },
-        web: { all(target_os = "unknown", target_arch = "wasm32") },
+        web: { target_arch = "wasm32" },
     }
 }
--- examples/template/build.rs
+++ examples/template/build.rs
@@ -58,9 +58,7 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=./dev/");
 
     cfg_aliases! {
-        windows: { all(target_os = "windows", target_arch = "x86_64") },
-        unix: { all(target_os = "linux", target_arch = "x86_64") },
-        web: { all(target_os = "unknown", target_arch = "wasm32") },
+        web: { target_arch = "wasm32" },
     }
 
     Ok(())

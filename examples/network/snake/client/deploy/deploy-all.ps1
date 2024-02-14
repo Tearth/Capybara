9d531b736da2c45729a12fc6e487eeb2aaeaeebd
Tearth <tearthdev@gmail.com>
2024-02-14 22:18:12 +0100

    Remove unused deploy scripts

--- examples/network/snake/client/deploy/deploy-all.ps1
+++
@@ -1,3 +0,0 @@
-& .\deploy\deploy-windows.ps1
-& .\deploy\deploy-linux.ps1
-& .\deploy\deploy-web.ps1
--- examples/network/snake/client/deploy/deploy-itchio.ps1
+++
@@ -1,6 +0,0 @@
-$NAME = "Test/Test"
-$VERSION = "1.0.0"
-
-Start-Process -FilePath "butler" -ArgumentList "push .\target\deploy\windows.zip $NAME`:windows --userversion $VERSION" -Wait -NoNewWindow
-Start-Process -FilePath "butler" -ArgumentList "push .\target\deploy\linux.zip $NAME`:linux --userversion $VERSION" -Wait -NoNewWindow
-Start-Process -FilePath "butler" -ArgumentList "push .\target\deploy\web.zip $NAME`:web --userversion $VERSION" -Wait -NoNewWindow
